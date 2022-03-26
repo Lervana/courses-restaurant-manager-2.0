9fb88fdd412bf41eb19be8656925a8534553adef
Katarzyna Dadek <k.dadek@lervana.dev>
2022-03-27 00:04:49 +0100

    [04] Create config file and logger

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+import _ from 'lodash'
+
+import { TConfig, TEnv } from 'src/types/config.types'
+
+const noEnvFile = process.env.NO_ENVFILE
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+if (_.isNil(noEnvFile)) require('dotenv').config()
+
+const env = (process.env.NODE_ENV || 'production') as TEnv
+const isDev = env === 'development'
+const isTest = env === 'test'
+
+const config: TConfig = {
+    env,
+    isDev,
+    isTest,
+    name: 'Example app',
+    options: {
+        port: Number(process.env.APP_PORT) || 3000,
+        logLevel: isDev ? 'silly' : 'http',
+        silentLogs: isTest,
+    },
+}
+
+export default config
